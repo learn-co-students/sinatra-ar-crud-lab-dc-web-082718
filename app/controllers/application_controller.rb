2d86dd3d6c8faf47fc7747bbeb9998f039006899
Srikant Kumar Kalaputapu <sriki@Srikants-MacBook-Pro.local>
2018-09-19 15:53:24 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,56 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
+      erb :index
+  end
+
+  get '/posts' do
+      @posts = Post.all
+      erb :index
+  end
+
+  post "/posts" do
+      name = params[:name]
+      content = params[:content]
+      @post = Post.create(name: name,content: content)
+      redirect "/posts"
+  end
+
+  get "/posts/new" do
+      erb :new
+  end
+
+  get "/posts/:id" do
+      @post = Post.find(params[:id])
+      # binding.pry
+      erb :show
+  end
+
+  patch "/posts/:id" do
+      post = Post.find(params[:id])
+      # binding.pry
+      post.name = params[:name]
+      post.content = params[:content]
+      post.save
+
+    redirect "posts/#{post.id}"
+  end
+
+  get "/posts/:id/edit" do
+      @post = Post.find(params[:id])
+      # binding.pry
+      erb :edit
+  end
+
+  delete "/posts/:id/delete" do
+      post = Post.find(params[:id])
+      post.destroy
+      redirect "/posts"
 
   end
+
 end
